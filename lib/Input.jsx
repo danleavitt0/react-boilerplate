43cb3a61f9ef26058990923a600067c5edf049c9
Daniel <danleavitt0@gmail.com>
2015-05-11 14:17:35 -0700

    form require

---
+++ lib/Input.jsx
@@ -0,0 +1,49 @@
+var React = require('react'),
+		mui = require('material-ui'),
+		TextField = mui.TextField
+
+var Input = React.createClass({
+
+	getInitialState: function () {
+    return {
+      value: this.props.value || ''
+    }
+  },
+
+  componentWillMount: function () {
+    this.props.attachToForm(this)
+  },
+
+  componentWillUnmount: function () {
+    this.props.detachFromForm(this)
+  },
+
+  setErrorText: function () {
+  	this.setState({
+  		errorText: 'This is a required field'
+  	})
+  },
+  
+  setValue: function (event) {
+    this.setState({
+      value: event.currentTarget.value,
+      errorText: null
+    })
+  },
+
+	render: function () {
+		return (
+			<TextField 
+				name={this.props.name} 
+				onChange={this.setValue} 
+				value={this.state.value} 
+				hintText={this.props.hintText}
+				multiLine={this.props.multiLine}
+				errorText={this.state.errorText}
+			/>
+		)
+	}
+
+})
+
+module.exports = Input
